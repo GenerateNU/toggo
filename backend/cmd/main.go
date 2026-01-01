b914abd5ac5d7dfd3f7fa17e282fac3f5da3b72b
Mai Nguyen <123816878+in-mai-space@users.noreply.github.com>
2026-01-01 16:35:57 -0500

    setup: database migration and config (#2)

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"toggo/internal/config"
+	"toggo/internal/database"
+
+	"github.com/uptrace/bun"
 )
 
 func main() {
+	cfg := mustLoadConfig()
+	db := mustConnectDB(context.Background(), cfg)
+	defer closeDB(db)
+}
+
+func mustLoadConfig() *config.Configuration {
 	cfg, err := config.LoadConfiguration()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
-	fmt.Printf("Loaded configuration\n", cfg)
+	return cfg
+}
+
+func mustConnectDB(ctx context.Context, config *config.Configuration) *bun.DB {
+	db, err := database.NewDB(ctx, config.Database.DSN())
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	log.Println("Connected to database successfully!")
+	return db
+}
+
+func closeDB(db *bun.DB) {
+	if err := db.Close(); err != nil {
+		log.Printf("Failed to close DB: %v", err)
+	}
 }
