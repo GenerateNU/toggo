45c64f915d4dbbc174ead460fcc9027a54d0c456
Mai Nguyen <123816878+in-mai-space@users.noreply.github.com>
2026-01-03 01:44:50 -0500

    setup: frontend, nativewind and svg transformer (#7)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",  // Expo Router app folder
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
