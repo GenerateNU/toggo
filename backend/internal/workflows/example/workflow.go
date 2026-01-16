1f89cd2437f3518b13a886b8df80b6ecac58acf9
mai <thisismainguyen@gmail.com>
2026-01-16 14:40:42 -0500

    setup workflow client

--- backend/internal/workflows/example/workflow.go
+++ backend/internal/workflows/example/workflow.go
@@ -3,6 +3,7 @@ package example
 import (
 	"time"
 	"toggo/internal/models"
+	"toggo/internal/repository"
 
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
@@ -11,17 +12,11 @@ import (
 func UserWorkflow(
 	ctx workflow.Context,
 	input UserWorkflowInput,
+	repository *repository.Repository,
 ) error {
-
 	logger := workflow.GetLogger(ctx)
-	activities := &UserActivities{}
+	activities := &UserActivities{Repository: repository}
 
-	// Helper to execute activity with retry and capture result
-	executeActivity := func(ctx workflow.Context, act interface{}, result interface{}, args ...interface{}) error {
-		return workflow.ExecuteActivity(ctx, act, args...).Get(ctx, result)
-	}
-
-	// create user
 	createCtx := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
 		StartToCloseTimeout: 10 * time.Second,
 		RetryPolicy: &temporal.RetryPolicy{
@@ -33,13 +28,11 @@ func UserWorkflow(
 	})
 
 	var created *models.User
-	if err := executeActivity(createCtx, activities.CreateUser, &created, input.User); err != nil {
+	if err := workflow.ExecuteActivity(createCtx, activities.CreateUser, input.User).Get(createCtx, &created); err != nil {
 		logger.Error("CreateUser failed", "error", err)
 		return err
 	}
-	logger.Info("User created", "userID", created.ID)
 
-	// update user
 	updateCtx := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
 		StartToCloseTimeout: 5 * time.Second,
 		RetryPolicy: &temporal.RetryPolicy{
@@ -51,13 +44,11 @@ func UserWorkflow(
 	})
 
 	var updated *models.User
-	if err := executeActivity(updateCtx, activities.UpdateUser, &updated, created.ID, input.UpdateReq); err != nil {
+	if err := workflow.ExecuteActivity(updateCtx, activities.UpdateUser, created.ID, input.UpdateReq).Get(updateCtx, &updated); err != nil {
 		logger.Error("UpdateUser failed", "error", err)
 		return err
 	}
-	logger.Info("User updated", "userID", updated.ID)
 
-	// delete
 	deleteCtx := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
 		StartToCloseTimeout: 5 * time.Second,
 		RetryPolicy: &temporal.RetryPolicy{
@@ -68,11 +59,10 @@ func UserWorkflow(
 		},
 	})
 
-	if err := executeActivity(deleteCtx, activities.DeleteUser, nil, updated.ID); err != nil {
+	if err := workflow.ExecuteActivity(deleteCtx, activities.DeleteUser, nil, updated.ID).Get(deleteCtx, nil); err != nil {
 		logger.Error("DeleteUser failed", "error", err)
 		return err
 	}
-	logger.Info("User deleted", "userID", updated.ID)
 
 	return nil
 }
