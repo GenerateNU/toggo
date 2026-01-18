b50621b0be42a57024e10206996b03aebff2831e
Bartavius <lojanarungsiri.j@northeastern.edu>
2026-01-18 15:10:26 -0500

    added profile pic migration

--- backend/internal/models/users.go
+++ backend/internal/models/users.go
@@ -5,9 +5,10 @@ import (
 )
 
 type User struct {
-	ID       uuid.UUID `bun:"id,pk,type:uuid" json:"id"`
-	Name     string    `bun:"name" json:"name"`
-	Username string    `bun:"username" json:"username"`
+	ID       		uuid.UUID 		`bun:"id,pk,type:uuid" json:"id"`
+	Name     		string    		`bun:"name" json:"name"`
+	Username 		string    		`bun:"username" json:"username"`
+	ProfilePicture  *string 		`bun:"profile_picture" json:"profile_picture,omitempty"`
 }
 
 type CreateUserRequest struct {
