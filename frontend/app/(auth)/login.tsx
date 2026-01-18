c489f1a17276a5613de70dda71802c7a821d3989
mai <thisismainguyen@gmail.com>
2026-01-18 02:39:44 -0500

    format

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -4,7 +4,12 @@ import { PhoneNumberForm } from "./components/login-form";
 
 export default function PhoneNumberPage() {
   return (
-    <Box flex={1} backgroundColor="cloudWhite" justifyContent="center" padding="l">
+    <Box
+      flex={1}
+      backgroundColor="cloudWhite"
+      justifyContent="center"
+      padding="l"
+    >
       <Text variant="bodyLargeBold" mb="l">
         Login with phone number
       </Text>
