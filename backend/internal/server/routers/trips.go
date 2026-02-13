19dafc8746d755818b2ccc34e83a4cdbd3c22e68
amoghathimamula <amoghathimamula@gmail.com>
2026-02-13 17:29:06 -0500

    feat: add trip_pitches table, models, and pitch repository

--- backend/internal/server/routers/trips.go
+++ backend/internal/server/routers/trips.go
@@ -17,6 +17,15 @@ func TripRoutes(apiGroup fiber.Router, routeParams types.RouteParams) fiber.Rout
 	)
 	tripController := controllers.NewTripController(tripService, routeParams.Validator)
 
+	awsCfg := routeParams.ServiceParams.Config.AWS
+	pitchService := services.NewPitchService(services.PitchServiceConfig{
+		PresignClient:  awsCfg.PresignClient,
+		PitchRepo:      routeParams.ServiceParams.Repository.Pitch,
+		MembershipRepo: routeParams.ServiceParams.Repository.Membership,
+		BucketName:     awsCfg.BucketName,
+	})
+	pitchController := controllers.NewPitchController(pitchService, routeParams.Validator)
+
 	// /api/v1/trips
 	tripGroup := apiGroup.Group("/trips")
 	tripGroup.Post("", tripController.CreateTrip)
@@ -28,6 +37,13 @@ func TripRoutes(apiGroup fiber.Router, routeParams types.RouteParams) fiber.Rout
 	tripIDGroup.Get("", tripController.GetTrip)
 	tripIDGroup.Patch("", tripController.UpdateTrip)
 	tripIDGroup.Delete("", tripController.DeleteTrip)
+
+	// /api/v1/trips/:tripID/pitches
+	tripIDGroup.Post("/pitches", pitchController.CreatePitch)
+	tripIDGroup.Get("/pitches", pitchController.ListPitches)
+	tripIDGroup.Get("/pitches/:pitchID", pitchController.GetPitch)
+	tripIDGroup.Patch("/pitches/:pitchID", pitchController.UpdatePitch)
+	tripIDGroup.Delete("/pitches/:pitchID", pitchController.DeletePitch)
 
 	return tripGroup
 }
