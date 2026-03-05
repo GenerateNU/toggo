1ba40f156ea051f9a411659f5cd99e942e4e1adf
mai <thisismainguyen@gmail.com>
2026-03-05 01:12:12 -0800

    queries to update categories

--- backend/internal/repository/poll_category.go
+++ backend/internal/repository/poll_category.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"github.com/uptrace/bun"
 )
 
@@ -22,5 +23,18 @@ func NewPollCategoryRepository(db *bun.DB) PollCategoryRepository {
 }
 
 func (r *pollCategoryRepository) ReplaceCategoriesForPoll(ctx context.Context, tx bun.Tx, tripID, pollID uuid.UUID, categories *[]string) ([]string, error) {
-	return []string{}, nil
+	var names []string
+	query := `
+        WITH ins AS (
+            INSERT INTO poll_categories (poll_id, category_name)
+            SELECT $1, c
+            FROM unnest(CASE WHEN $2 IS NULL OR array_length($2, 1) = 0 THEN ARRAY[]::text[] ELSE $2 END) AS c
+            ON CONFLICT (poll_id, category_name) DO NOTHING
+        )
+        SELECT category_name
+        FROM poll_categories
+        WHERE poll_id = $1
+    `
+	err := tx.NewRaw(query, pollID, pq.Array(categories)).Scan(ctx, &names)
+	return names, err
 }
