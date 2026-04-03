b4d370ec34a68ae30521f1aa74102e3b9e7b886a
Anish Sahoo <sahoo.an@northeastern.edu>
2026-04-02 20:51:22 -0400

    Add code

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -58,6 +58,10 @@ const resolveBaseUrl = (): string => {
     return `${protocol}//${hostname}:8000`;
   }
 
+  if (Platform.OS === "android") {
+    return "http://10.0.2.2:8000";
+  }
+
   return "http://localhost:8000";
 };
 
