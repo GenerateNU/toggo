ad58c5211c08852106792b4f6d8bb92430198eca
Bartavius <lojanarungsiri.j@northeastern.edu>
2026-04-13 22:18:37 -0400

    fix for empty state

--- frontend/app/(app)/trips/[id]/itinerary-tab/components/itinerary-tab-content.tsx
+++ frontend/app/(app)/trips/[id]/itinerary-tab/components/itinerary-tab-content.tsx
@@ -52,7 +52,7 @@ export function ItineraryTabContent({
   });
 
   if (!startDateKey || !endDateKey) {
-    return <ItineraryEmptyState hasDates={false} />;
+    return <ItineraryEmptyState />;
   }
 
   return (
