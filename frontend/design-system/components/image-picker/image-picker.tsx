182eca14ca38ab2833aad7953476502b7bcdcd1d
mai <thisismainguyen@gmail.com>
2026-04-15 08:06:41 -0400

    small fix

--- frontend/design-system/components/image-picker/image-picker.tsx
+++ frontend/design-system/components/image-picker/image-picker.tsx
@@ -7,7 +7,14 @@ import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
 import * as DocumentPicker from "expo-document-picker";
 import { Image } from "expo-image";
 import * as ExpoImagePicker from "expo-image-picker";
-import { ImagePlus, Images, Image as LucideImage, Trash2, Upload, X } from "lucide-react-native";
+import {
+  ImagePlus,
+  Images,
+  Image as LucideImage,
+  Trash2,
+  Upload,
+  X,
+} from "lucide-react-native";
 import React, { useCallback, useRef } from "react";
 import {
   Alert,
